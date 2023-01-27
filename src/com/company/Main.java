0e3d23bff61c449268e867ea37defc7a4ae19195
Pezentrias <102146195+Pezentrias@users.noreply.github.com>
2023-01-27 10:05:20 +0100

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/world";
+        String dbUser = "root";
+        String dbPass = "1234";
+        Scanner sca = new Scanner(System.in);
+
+    }
+}
